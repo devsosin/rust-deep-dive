5ddec7137879fbe3314f08c2fc5a606c2c5f120f
sosin <svstar94@gmail.com>
2025-02-17 22:18:47 +0900

    appplication state for pg connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 
 use zero2prod::config::get_config;
@@ -6,8 +7,11 @@ use zero2prod::startup::run;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let config = get_config().expect("Failed to read configuration.");
+    let connection = PgConnection::connect(&config.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
 
     let address = format!("127.0.0.1:{}", config.app_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,23 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
-use crate::routes::{health_check, subscription};
+use crate::routes::{health_check, subscribe};
 
-pub fn run(listner: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listner: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    // web::Data wraps in an Atomic Reference Counted pointer
+    // Arc는 원본을 복사해서 전달하는 것이 아닌 pointer를 전달
+    // active 참조 카운트를 1 증가시키고 감싸고있는 값의 메모리 주소 복사를 전달
+    // Wrap the connection in a smart pointer
+    let connection = web::Data::new(connection);
+    // Capture `connection` from the surrounding environment
+    let server = HttpServer::new(move || {
         App::new()
-            .route("/health_check", web::get().to(health_check::health_check))
-            .route("/subscriptions", web::post().to(subscription::subscribe))
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+            // get pointer copy
+            .app_data(connection.clone())
     })
     .listen(listner)?
     .run();
