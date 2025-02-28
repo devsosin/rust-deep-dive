284524b11c78e9cb187d6b1c3c3880ec8eec68f6
sosin <svstar94@gmail.com>
2025-02-28 14:42:45 +0900

    add log

--- src/routes/subscription.rs
+++ src/routes/subscription.rs
@@ -27,6 +27,8 @@ pub struct FormData {
 // shared refrence to PgPool (pool of connections)
 // pool에서 PgConnection을 가져오고, 가용하지 않으면 새로 생성하거나 free up 할 때까지 기다림
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    log::info!("Saving new subscriber details in the database");
+
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -40,9 +42,12 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .execute(pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!("New subscriber details have been saved");
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            log::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
